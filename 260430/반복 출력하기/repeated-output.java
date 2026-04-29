8813e5c2ddbe194aeada302b6c2b52db8ab1f3a8
HappyGogildong <rbgus630@gmail.com>
2026-04-30 01:56:00 +0900

    Title: 반복 출력하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/반복 출력하기/repeated-output.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void printer(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println("12345^&*()_");
+        }
+    }
+
+    public static void main(String[] args) {
+        printer(4);
+    }
+}
